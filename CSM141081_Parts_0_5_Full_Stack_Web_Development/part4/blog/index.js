ef3a9e78d9fb74e63b8b4f2e809ad31ee30b1a21
Animus <lovro@x-network.io>
2023-12-08 19:44:03 +0200

    some pushes

--- CSM141081_Parts_0_5_Full_Stack_Web_Development/part4/blog/index.js
+++
@@ -1,8 +0,0 @@
-const config = require('./utils/config')
-const app = require('./app')
-const logger = require('./utils/logger')
-
-app.listen(config.PORT, () =>{
-  logger.info(`Server running on port ${config.PORT} and mongodb connection to ${config.MONGODB_URI}`)
-})
-
